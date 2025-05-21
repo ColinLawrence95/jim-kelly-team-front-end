11eaed20a753aec4add5c56c5b37077b1740e789
Colin <colin.lawrence95@gmail.com>
2025-05-21 17:46:26 -0400

    lisitng

--- src/components/CurrentListings/CurrentListings.tsx
+++ src/components/CurrentListings/CurrentListings.tsx
@@ -12,6 +12,7 @@ function CurrentListings() {
             <FaArrowLeftLong className="current-listings-arrow-left" size={40} />
             {listings.map((listing, index) => (
                 <div key = {index} className="current-listings-element-container">
+                    <div>Listing #{listing}</div>
                     <div
                         key={index}
                         className="current-listings-element"
