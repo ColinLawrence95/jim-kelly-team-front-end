15d64da7b460180d3122eac1ca61825fedcdb137
Colin <colin.lawrence95@gmail.com>
2025-05-21 17:49:15 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import NavBar from "./components/NavBar/NavBar";
-import Footer from "./components/Footer/Footer";
-import LandingPage from "./components/LandingPage/LandingPage";
-import AboutPage from "./components/AboutPage/AboutPage";
-import { Route, Routes } from "react-router-dom";
+// import NavBar from "./components/NavBar/NavBar";
+// import Footer from "./components/Footer/Footer";
+// import LandingPage from "./components/LandingPage/LandingPage";
+// import AboutPage from "./components/AboutPage/AboutPage";
+// import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
     return (
         <>
-            <NavBar />
+        <h1>UNDER CONSTRUCTION</h1>
+            {/* <NavBar />
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/about" element={<AboutPage />} />
             </Routes>
-            <Footer />
+            <Footer /> */}
         </>
     );
 }
