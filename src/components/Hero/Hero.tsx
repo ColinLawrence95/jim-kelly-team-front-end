9e17b1f31c717647ae9b4eb0ecf1872a6e8bdbb3
Colin <colin.lawrence95@gmail.com>
2025-07-02 20:54:00 -0400

    hero styling

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,18 +1,20 @@
 import "./Hero.css";
 function Hero() {
     return (
-        <div className="hero-container">
-            <div className="hero-title-container">
-                <div className="hero-title-and-button">
-                    <h2>The Jim Kelly Team</h2>
+        <div className="hero-page-container" style={{ backgroundImage: `url(/house2.png)` }}>
+            <div className="hero-container" >
+                <div className="hero-title-container">
+                    <div className="hero-title-and-button">
+                        <h2 id="hero-title">The Jim Kelly Team</h2>
 
-                    <p id="hero-tagline">From Our Family to Yours</p>
+                        <p id="hero-tagline">From Our Family to Yours</p>
+                    </div>
                 </div>
+                <div
+                    className="hero-team-image"
+                    style={{ backgroundImage: `url(/test.png)` }}
+                ></div>
             </div>
-            <div
-                className="hero-team-image"
-                style={{ backgroundImage: `url(/test.png)` }}
-            ></div>
         </div>
     );
 }
